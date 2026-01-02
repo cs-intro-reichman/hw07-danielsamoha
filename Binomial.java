0eab816d47a8b1460df068233261c9d94d80b520
Daniel Samoha <danielaharon.samoha@post.runi.ac.il>
2026-01-02 20:26:26 +0200

    update

--- Binomial.java
+++ Binomial.java
@@ -4,7 +4,7 @@ public class Binomial {
 		//// Uncomment the version of binomial that you want to test
  
 		// Testing the basic binomial implementation:
-    	// System.out.println(binomial1(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+    	System.out.println(binomial1(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
 
 		// Testing the optimized binomial implementation:
 		System.out.println(binomial(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
@@ -13,7 +13,13 @@ public class Binomial {
 	// Computes the Binomial function, basic version.
 	public static int binomial1(int n, int k) { 
 		//// Repplace the following comment with your code
-		return 0;
+		if (k > n){
+			return 0;
+		}
+        if (k == 0 || k == n){
+			return 1;
+		}
+        return binomial1(n - 1, k) + binomial1(n - 1, k - 1);
 	 }
 	
 	// Computes the Binomial function, efficiently
@@ -22,6 +28,13 @@ public class Binomial {
 		//// and then initializes all its elements to -1.
 		//// It then calls binomial(n, k, memo), which does all the heavy lifiting.
 		//// Replace the following statement with your code.
+		if (k > n){
+			return 0;
+		}
+        if (k < 0){
+			return 0;
+		}
+		
 		int [][] memo = new int[n + 1][k + 1];
 		for (int i = 0; i < n + 1 ;i++){
 			for (int j = 0; j < k + 1 ;j++){
