5cbb7d7f74562c83234a2305082c1bc558085bf1
Daniel Samoha <danielaharon.samoha@post.runi.ac.il>
2026-01-02 20:43:58 +0200

    update

--- Binomial.java
+++ Binomial.java
@@ -11,7 +11,7 @@ public class Binomial {
 	}
 
 	// Computes the Binomial function, basic version.
-	public static int binomial1(int n, int k) { 
+	public static long binomial1(int n, int k) { 
 		//// Repplace the following comment with your code
 		if (k > n){
 			return 0;
@@ -23,7 +23,7 @@ public class Binomial {
 	 }
 	
 	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
+	public static long binomial(int n, int k) {
 		//// This function creates a 2D array, say memo, 
 		//// and then initializes all its elements to -1.
 		//// It then calls binomial(n, k, memo), which does all the heavy lifiting.
@@ -35,7 +35,7 @@ public class Binomial {
 			return 0;
 		}
 		
-		int [][] memo = new int[n + 1][k + 1];
+		long [][] memo = new long[n + 1][k + 1];
 		for (int i = 0; i < n + 1 ;i++){
 			for (int j = 0; j < k + 1 ;j++){
 				memo[i][j] = -1;
@@ -44,7 +44,7 @@ public class Binomial {
 		return binomial(n, k, memo);
 	}
 
-	private static int binomial(int n, int k, int[][] memo) {
+	private static long binomial(int n, int k, long[][] memo) {
 		if (memo[n][k] != -1) {
 			return memo[n][k];
 		}
